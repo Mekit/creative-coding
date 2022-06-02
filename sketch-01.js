d5247f1bdaaf8aa566d37b93151464470971723a
Luca Cattaneo <luca.cattaneo@mekit.it>
2022-06-02 11:05:39 +0200

    Colors

--- sketch-01.js
+++ sketch-01.js
@@ -4,9 +4,15 @@ const settings = {
   dimensions: [ 1024, 1024 ]
 };
 
+const colors = [
+  '#495ED6', // Blue
+  '#F94E50', // Red
+  '#FFC700', // Yellow
+];
+
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = colors[2];
     context.fillRect(0, 0, width, height);
 
     // Variables
@@ -16,17 +22,9 @@ const sketch = () => {
     let unit = gridDimension * 0.1;
     let squareGap = unit * 1.5;
 
-    // Colors
-    let colors = [
-      'black',
-      'red',
-      'white'
-    ];
-
     context.strokeStyle = 'gray';
     context.setLineDash([unit * 0.5, unit * 1]);
     context.lineWidth = unit * 0.25;
-    
 
     for (let i = 0; i < gridCount; i++) {
       for (let j = 0; j < gridCount; j++) {
@@ -36,24 +34,21 @@ const sketch = () => {
         // Grid
         context.beginPath();
         context.rect(x, y, gridDimension, gridDimension);
-        context.stroke();
+        // context.stroke();
 
-        // Color random
-        // let number = Math.floor(Math.random() * 3);
+        // Random color
+        // let number = Math.floor(Math.random() * colors.length);
         // let color = colors[number];
-        // context.fillStyle = color;
 
-        let color = colors[0];
+        // Controlled color
+        let color = 'black';
         let number = Math.random();
         if (number > 0.9){
           color = colors[2];
         }
-        // if (number < 0.1){
-        //   color = colors[2];
-        // }
-        context.fillStyle = color;
         
         // Filled Square
+        context.fillStyle = color;
         context.fillRect(
           x + squareGap * 0.5,
           y + squareGap * 0.5,
@@ -62,21 +57,6 @@ const sketch = () => {
         );
       }
     }
-
-    
-
-    // context.beginPath();
-    // context.strokeStyle = 'red';
-    // context.lineWidth = 10;
-    // context.rect(400, 100, 200, 200);
-    // context.stroke();
-
-    // context.beginPath();
-    // context.lineTo(900, 700);
-    // context.lineTo(700, 800);
-    // context.lineTo(200, 800);
-    // context.fill();
-    // context.stroke();
   };
 };
 
